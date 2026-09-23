f649f4439ac059c94a9023fe34a80a19dae44a2b
stty0219 <131657111+stty0219@users.noreply.github.com>
2026-09-23 18:12:21 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+const CACHE='fenguan-v1';
+const CORE=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET')return;
+  e.respondWith(
+    fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});return res})
+    .catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html')))
+  );
+});
